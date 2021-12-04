8c99698349f9d6c785f19ff7b4d94b6610f73d1a
Stefan Schotvanger <s.schotvanger@gmail.com>
2021-12-04 12:23:24 +0100

    basis html structure

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
+<html lang="en">
+<head>
+    <title>NScot - bookmarlets</title>
+</head>
+<body>
+    <header>
 
-phpinfo();
+    </header>
+    <section>
+        <article>
+            <h2>Download youtube video</h2>
+            <p>
+                Setting this script will instantly downloaqd the youtube video from the youtube page you are viewing.
+            </p>
+            <pre>
+                javascrip:(function(){console.clear;console.log('test')})()
+            </pre>
+
+        </article>
+    </section>
+    <aside>
+
+    </aside>
+    <footer>
+
+    </footer>
+</body>
+</html>
